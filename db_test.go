8d1f79ccfb3d32c717b472df551f9f470bf20f55
Luis Jakón <luisjakon@gmail.com>
2019-10-31 20:47:44 -0400

    Place tx tests on separate file

--- db_test.go
+++ db_test.go
@@ -2,10 +2,6 @@ package cqlx_test
 
 import (
 	"testing"
-
-	"github.com/luisjakon/cqlx"
-	"github.com/scylladb/gocqlx/qb"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDB(t *testing.T) {
@@ -30,61 +26,4 @@ func TestDB(t *testing.T) {
 			t.Error("Invalid table")
 		}
 	}
-}
-
-func TestDBViewTx(t *testing.T) {
-
-	get := qb.Select("kv").Where(qb.Eq("key")).Limit(1)
-
-	db.View(func(tx cqlx.Tx) error {
-
-		res := kv{}
-		err := tx.Queryx(get, "4").Get(&res)
-
-		assert.Equal(t, nil, err)
-		assert.Equal(t, "4", res.Key)
-		assert.Equal(t, "val4", res.Value)
-
-		return err
-	})
-
-}
-
-func TestDBUpdateTx(t *testing.T) {
-
-	get := qb.Select("kv").Where(qb.Eq("key"))
-	update := qb.Update("kv").Where(qb.Eq("key")).Set("value")
-	delete := qb.Delete("kv").Where(qb.Eq("key"))
-
-	// Upsert record
-	db.Update(func(tx cqlx.Tx) error {
-
-		err := tx.Queryx(update).Put(&kv{Key: "5", Value: "val5"})
-		assert.Nil(t, err)
-
-		return err
-	})
-
-	// Retrieve and compare
-	db.View(func(tx cqlx.Tx) error {
-
-		res := kv{}
-		err := tx.Queryx(get, "5").Get(&res)
-
-		assert.Equal(t, nil, err)
-		assert.Equal(t, "5", res.Key)
-		assert.Equal(t, "val5", res.Value)
-
-		return err
-	})
-
-	// Remove record
-	db.Update(func(tx cqlx.Tx) error {
-
-		err := tx.Queryx(delete, "5").Exec()
-		assert.Equal(t, nil, err)
-
-		return err
-	})
-
 }
